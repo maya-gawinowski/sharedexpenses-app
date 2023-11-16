28419e302bf85f19ecc87102ce030959c946300a
kpoudel04 <kiran4poudel@gmail.com>
2023-11-16 21:01:00 +0100

    JoinGroupPage

--- SharedExpensesApp/app/build.gradle.kts
+++ SharedExpensesApp/app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.navigation:navigation-compose:2.7.0")
+    implementation("com.google.firebase:firebase-inappmessaging-ktx:20.4.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- SharedExpensesApp/app/src/main/java/com/example/sharedexpensesapp/ui/screens/CreateGroupScreen.kt
+++ SharedExpensesApp/app/src/main/java/com/example/sharedexpensesapp/ui/screens/CreateGroupScreen.kt
@@ -1,5 +1,7 @@
 package com.example.sharedexpensesapp.ui.screens
 
+import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,9 +11,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -20,19 +31,27 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import com.example.sharedexpensesapp.R
+import com.example.sharedexpensesapp.model.GroupItem
 
 @Composable
-fun AddGroupScreen(modifier: Modifier) {
+fun CreateGroupScreen() {
     var groupName by remember { mutableStateOf(TextFieldValue("")) }
     var groupDescription by remember { mutableStateOf(TextFieldValue("")) }
-    var participantName by remember { mutableStateOf("") }
-    var participants by remember { mutableStateOf(listOf("You")) }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -95,44 +114,48 @@ fun AddGroupScreen(modifier: Modifier) {
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        // List of participants
+        // Placeholder for the participants list, which should be replaced with an actual implementation
         Text(
             text = "Participants",
             fontWeight = FontWeight.Bold,
             fontSize = 20.sp,
             modifier = Modifier.align(Alignment.Start)
         )
-        participants.forEach { name ->
-            Text(text = name, modifier = Modifier.padding(8.dp))
-        }
 
-        Spacer(modifier = Modifier.height(8.dp))
-
-        // Add Participant
+        // Placeholder for the user name, which should be replaced with an actual implementation
         Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(vertical = 8.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            TextField(
-                value = participantName,
-                onValueChange = { participantName = it },
-                singleLine = true,
-                label = { Text("Enter a name") },
+            Text(
+                text = "You (user name)",
                 modifier = Modifier.weight(1f)
             )
-            Button(
-                onClick = {
-                    if (participantName.isNotBlank()) {
-                        participants = participants + participantName
-                        participantName = ""
+            // This should be replaced with an actual switch or checkbox
+            Text(text = "[Toggle Switch]")
+        }
+
+        // Placeholder for the add participant input, which should be replaced with an actual implementation
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            BasicTextField(
+                value = TextFieldValue(""),
+                onValueChange = {},
+                modifier = Modifier.weight(1f),
+                decorationBox = { innerTextField ->
+                    Box(contentAlignment = Alignment.CenterStart) {
+                        Text("Enter a name", color = Color.Gray)
+                        innerTextField()
                     }
-                },
-                modifier = Modifier.padding(start = 8.dp)
-            ) {
-                Text(text = "+")
-            }
+                }
+            )
+            Text(text = "+", modifier = Modifier.padding(start = 8.dp))
         }
 
         Spacer(modifier = Modifier.height(16.dp))
@@ -147,7 +170,4 @@ fun AddGroupScreen(modifier: Modifier) {
         }
     }
 }
-
-
-
 
