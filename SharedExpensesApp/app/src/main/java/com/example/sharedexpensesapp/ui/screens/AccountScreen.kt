b8593409d662f487b1bcb81b29a6c6b490f60992
MancaVavpotic <mv3237@student.uni-lj.si>
2023-11-22 01:02:12 +0100

    my account page first version

--- SharedExpensesApp/app/src/main/java/com/example/sharedexpensesapp/ui/screens/AccountScreen.kt
+++ SharedExpensesApp/app/src/main/java/com/example/sharedexpensesapp/ui/screens/AccountScreen.kt
@@ -1,8 +1,112 @@
 package com.example.sharedexpensesapp.ui.screens
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.sharedexpensesapp.R
 
 @Composable
 fun AccountScreen() {
+    var name by remember { mutableStateOf("") }
+    var nickname by remember { mutableStateOf("") }
+    var userEmail by remember { mutableStateOf("user@example.com") }
+    var notificationsEnabled by remember { mutableStateOf(false) }
+    val focusManager = LocalFocusManager.current
+    val context = LocalContext.current
+    val density = LocalDensity.current.density
 
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(R.drawable.illustration_sans_titre_35),
+            contentDescription = "image de fond",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.matchParentSize()
+        )
+
+        // Welcome Box
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome")
+            Spacer(modifier = Modifier.height(8.dp))
+            Text("User Email: $userEmail")
+        }
+    }
+
+    // Input Fields
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        TextField(
+            value = name,
+            onValueChange = { name = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp),
+            label = { Text("Name") },
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(
+                onNext = { focusManager.moveFocus(FocusDirection.Down) }
+            )
+        )
+        TextField(
+            value = nickname,
+            onValueChange = { nickname = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp),
+            label = { Text("Nickname") },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = { focusManager.clearFocus() }
+            )
+        )
+
+        // Toggle Button for Notifications
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text("Receive Notifications", modifier = Modifier.alignByBaseline())
+            Spacer(modifier = Modifier.width(8.dp))
+            Switch(
+                checked = notificationsEnabled,
+                onCheckedChange = { notificationsEnabled = it },
+            )
+        }
+    }
 }
