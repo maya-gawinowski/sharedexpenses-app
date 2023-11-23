a8fdfa7094075c82028e352204b0629423666c37
MancaVavpotic <mv3237@student.uni-lj.si>
2023-11-23 01:13:10 +0100

    changed screen

--- SharedExpensesApp/app/src/main/java/com/example/sharedexpensesapp/ui/screens/AccountScreen.kt
+++ SharedExpensesApp/app/src/main/java/com/example/sharedexpensesapp/ui/screens/AccountScreen.kt
@@ -1,11 +1,15 @@
 package com.example.sharedexpensesapp.ui.screens
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.ui.tooling.preview.Preview
+
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -18,9 +22,13 @@ import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.sharedexpensesapp.R
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun AccountScreen() {
@@ -33,6 +41,7 @@ fun AccountScreen() {
     val density = LocalDensity.current.density
 
     Box(modifier = Modifier.fillMaxSize()) {
+
         Image(
             painter = painterResource(R.drawable.illustration_sans_titre_35),
             contentDescription = "image de fond",
@@ -40,73 +49,108 @@ fun AccountScreen() {
             modifier = Modifier.matchParentSize()
         )
 
-        // Welcome Box
+        // Input Fields
         Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
+                .fillMaxSize(),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text("Welcome")
-            Spacer(modifier = Modifier.height(8.dp))
-            Text("User Email: $userEmail")
+        ){
+
+
+            Card(
+
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .size(width = 300.dp, height = 250.dp)
+                    .padding(top = 16.dp),
+                border= BorderStroke(5.dp, colorResource(id = R.color.main_orange)),
+
+                backgroundColor = colorResource(id = R.color.main_purple),
+
+            )
+            {
+                Column (modifier = Modifier
+                    .fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally){
+                    Text(
+                        text = "Welcome user $name\n$userEmail",
+
+                        modifier = Modifier
+                            .height(150.dp)
+                            .fillMaxWidth()
+                            .fillMaxHeight()
+                            .padding(16.dp),
+                        style = TextStyle(fontSize = 30.sp),
+                        textAlign = TextAlign.Center,
+                        color = colorResource(id = R.color.main_orange),
+                    )
+                }
+
+            }
+
+            TextField(
+                value = name,
+                onValueChange = { name = it },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp),
+                label = { Text("Name") },
+
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Next
+                ),
+                keyboardActions = KeyboardActions(
+                    onNext = { focusManager.moveFocus(FocusDirection.Down) }
+                )
+            )
+            TextField(
+                value = nickname,
+                onValueChange = { nickname = it },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp),
+                label = { Text("Nickname") },
+                visualTransformation = PasswordVisualTransformation(),
+
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = { focusManager.clearFocus() }
+                )
+            )
+
+            // Toggle Button for Notifications
+            Spacer(modifier = Modifier.height(16.dp))
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text("Receive Notifications", modifier = Modifier.alignByBaseline() ,
+                        style = TextStyle(fontSize = 30.sp),
+                    textAlign = TextAlign.Center,
+                    color = colorResource(id = R.color.purple_700),)
+                Spacer(modifier = Modifier.width(15.dp))
+                Switch(
+                    checked = notificationsEnabled,
+                    onCheckedChange = { notificationsEnabled = it },
+                    colors = SwitchDefaults.colors(
+                        checkedThumbColor = colorResource(id = R.color.purple_200),
+                        uncheckedThumbColor = colorResource(id = R.color.purple_500),
+                        checkedTrackColor = colorResource(id = R.color.purple_700),
+                        uncheckedTrackColor =colorResource(id = R.color.purple_500),
+                    )
+                )
+            }
         }
+
+
+
     }
 
-    // Input Fields
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        TextField(
-            value = name,
-            onValueChange = { name = it },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 8.dp),
-            label = { Text("Name") },
-            keyboardOptions = KeyboardOptions(
-                imeAction = ImeAction.Next
-            ),
-            keyboardActions = KeyboardActions(
-                onNext = { focusManager.moveFocus(FocusDirection.Down) }
-            )
-        )
-        TextField(
-            value = nickname,
-            onValueChange = { nickname = it },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 8.dp),
-            label = { Text("Nickname") },
-            visualTransformation = PasswordVisualTransformation(),
-            keyboardOptions = KeyboardOptions(
-                imeAction = ImeAction.Done
-            ),
-            keyboardActions = KeyboardActions(
-                onDone = { focusManager.clearFocus() }
-            )
-        )
-
-        // Toggle Button for Notifications
-        Spacer(modifier = Modifier.height(16.dp))
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp),
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text("Receive Notifications", modifier = Modifier.alignByBaseline())
-            Spacer(modifier = Modifier.width(8.dp))
-            Switch(
-                checked = notificationsEnabled,
-                onCheckedChange = { notificationsEnabled = it },
-            )
-        }
-    }
 }
